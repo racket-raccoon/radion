72b62e47736e11606c48aaa5bdb832770355bd99
micahkepe <micahkepe@gmail.com>
2024-11-11 10:35:53 -0600

    Add theme toggle scripts and assets, update README

--- static/js/toggle-theme.js
+++ static/js/toggle-theme.js
@@ -2,10 +2,39 @@
  * Adapted from `apollo` theme:
  * https://github.com/not-matthias/apollo/blob/main/static/js/themetoggle.js
  */
+
+/**
+ * Update the theme mode in local storage.
+ * @param {string} mode - The theme mode to set ("light" or "dark")
+ * @returns {void}
+ */
 function setTheme(mode) {
   localStorage.setItem("theme-storage", mode);
 }
 
+/** Fetch the current theme mode from local storage or the browser settings.
+ * @returns {string} The current theme mode ("light" or "dark")
+ * */
+function getSavedTheme() {
+  let currentTheme = localStorage.getItem("theme-storage");
+  if (!currentTheme) {
+    if (
+      window.matchMedia &&
+      window.matchMedia("(prefers-color-scheme: dark)").matches
+    ) {
+      currentTheme = "dark";
+    } else {
+      currentTheme = "light";
+    }
+  }
+
+  return currentTheme;
+}
+
+/**
+ * Toggle the theme between light and dark mode
+ * @returns {void}
+ */
 function toggleTheme() {
   if (localStorage.getItem("theme-storage") === "light") {
     setTheme("dark");
@@ -16,6 +45,10 @@ function toggleTheme() {
   }
 }
 
+/**
+ * Update the theme UI icon based on the current theme mode.
+ * @returns {void}
+ */
 function updateItemToggleTheme() {
   let mode = getSavedTheme();
 
@@ -39,22 +72,6 @@ function updateItemToggleTheme() {
     htmlElement.classList.remove("dark");
     htmlElement.classList.add("light");
   }
-}
-
-function getSavedTheme() {
-  let currentTheme = localStorage.getItem("theme-storage");
-  if (!currentTheme) {
-    if (
-      window.matchMedia &&
-      window.matchMedia("(prefers-color-scheme: dark)").matches
-    ) {
-      currentTheme = "dark";
-    } else {
-      currentTheme = "light";
-    }
-  }
-
-  return currentTheme;
 }
 
 // Update the toggle theme on page load
